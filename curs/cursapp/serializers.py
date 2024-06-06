de29dd8bfcd745227bf2856e80603b01d38b0e1d
arlen <arlenismailov02@gmail.com>
2024-06-06 18:37:30 +0600

    serializers, urls, views, fixed

--- curs/cursapp/serializers.py
+++ curs/cursapp/serializers.py
@@ -1,39 +1,56 @@
 from .models import Focultet, Professor, Student, Course, Office, Schedule, Registrationcourse, Homework, Passingexam
 from rest_framework import serializers
 
+
 class FocultetSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Focultet
-		fields='all'
+    class Meta:
+        model = Focultet
+        fields = 'all'
+
+
 class ProfessorSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Professor
-		fields='all'
+    class Meta:
+        model = Professor
+        fields = 'all'
+
+
 class StudentSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Student
-		fields='all'
+    class Meta:
+        model = Student
+        fields = 'all'
+
+
 class CourseSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Course
-		fields='all'
+    class Meta:
+        model = Course
+        fields = 'all'
+
+
 class OfficeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Office
-		fields='all'
+    class Meta:
+        model = Office
+        fields = 'all'
+
+
 class ScheduleSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Schedule
-		fields='all'
+    class Meta:
+        model = Schedule
+        fields = 'all'
+
+
 class RegistrationcourseSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Registrationcourse
-		fields='all'
+    class Meta:
+        model = Registrationcourse
+        fields = 'all'
+
+
 class HomeworkSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=Homework
-		fields='all'
-class passingexamSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=passingexam
-		fields='all'
+    class Meta:
+        model = Homework
+        fields = 'all'
+
+
+class PassingexamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passingexam
+        fields = 'all'
--- curs/cursapp/views.py
+++ curs/cursapp/views.py
@@ -2,34 +2,50 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import Focultet, Professor, Student, Course, Office, Schedule, Registrationcourse, Homework, Passingexam
-from .serializers import FocultetSerializer, ProfessorSerializer, StudentSerializer, CourseSerializer, OfficeSerializer, ScheduleSerializer, RegistrationcourseSerializer, HomeworkSerializer, Passingexam
-
+from .serializers import FocultetSerializer, ProfessorSerializer, StudentSerializer, CourseSerializer, OfficeSerializer, \
+    ScheduleSerializer, RegistrationcourseSerializer, HomeworkSerializer, PassingexamSerializer
 
 
 class FocultetViewSets(viewsets.ModelViewSet):
-	queryset = Focultet.objects.all()
-	serializer_class = FocultetSerializer
+    queryset = Focultet.objects.all()
+    serializer_class = FocultetSerializer
+
+
 class ProfessorViewSets(viewsets.ModelViewSet):
-	queryset = Professor.objects.all()
-	serializer_class = ProfessorSerializer
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+
 class StudentViewSets(viewsets.ModelViewSet):
-	queryset = Student.objects.all()
-	serializer_class = StudentSerializer
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
 class CourseViewSets(viewsets.ModelViewSet):
-	queryset = Course.objects.all()
-	serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
 class OfficeViewSets(viewsets.ModelViewSet):
-	queryset = Office.objects.all()
-	serializer_class = OfficeSerializer
+    queryset = Office.objects.all()
+    serializer_class = OfficeSerializer
+
+
 class ScheduleViewSets(viewsets.ModelViewSet):
-	queryset = Schedule.objects.all()
-	serializer_class = ScheduleSerializer
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+
+
 class RegistrationcourseViewSets(viewsets.ModelViewSet):
-	queryset = Registrationcourse.objects.all()
-	serializer_class = RegistrationcourseSerializer
+    queryset = Registrationcourse.objects.all()
+    serializer_class = RegistrationcourseSerializer
+
+
 class HomeworkViewSets(viewsets.ModelViewSet):
-	queryset = Homework.objects.all()
-	serializer_class = HomeworkSerializer
-class passingexamViewSets(viewsets.ModelViewSet):
-	queryset = passingexam.objects.all()
-	serializer_class = passingexamSerializer
+    queryset = Homework.objects.all()
+    serializer_class = HomeworkSerializer
+
+
+class PassingexamViewSets(viewsets.ModelViewSet):
+    queryset = Passingexam.objects.all()
+    serializer_class = PassingexamSerializer
